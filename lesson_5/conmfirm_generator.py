e0c2d43308050c86ea75c1ca8ca9e29d209483a4
tanik12 <koyu.tani01@g.softbank.co.jp>
2019-12-07 23:39:37 +0900

    [update]Discriminatorの追加。その他ファイル名の変更。

--- lesson_5/conmfirm_generator.py
+++ lesson_5/conmfirm_generator.py
@@ -1,9 +1,16 @@
 import matplotlib.pyplot as plt
-from generator import Generator
+from gan import Generator, Discriminator
 
 import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import torchvision
+from torchvision import models, transforms
 
 if __name__ == "__main__":
+    #################
+    ### Generator ###
     G = Generator(z_dim=20, image_size=64)
 
     #入力する乱数
@@ -18,3 +25,20 @@ if __name__ == "__main__":
     img_transformed = fake_images[0][0].detach().numpy()
     plt.imshow(img_transformed, 'gray')
     plt.show()
+    #################
+
+    #####################
+    ### Discriminator ###
+    #動作確認
+    D = Discriminator(z_dim=20, image_size=64)
+
+    #偽画像を生成
+    input_z = torch.randn(1, 20)
+    input_z = input_z.view(input_z.size(0), input_z.size(1), 1, 1)
+
+    #偽画像をDに入力
+    d_out = D(fake_images)
+
+    #出力d_outにSigmoidをかけて0から1に変換
+    print(nn.Sigmoid()(d_out))
+    #####################
--- lesson_5/gan.py
+++ lesson_5/gan.py
@@ -57,3 +57,40 @@ class Generator(nn.Module):
         out = self.last(out)
 
         return out
+
+class Discriminator(nn.Module):
+
+    def __init__(self, z_dim=20, image_size=64):
+        super(Discriminator, self).__init__()
+
+        self.layer1 = nn.Sequential(
+                nn.Conv2d(1, image_size, kernel_size=4,
+                          stride=2, padding=1),
+                nn.LeakyReLU(0.1, inplace=True))
+        #注意：白黒画像なので入力チャネルは１つだけ
+
+        self.layer2 = nn.Sequential(
+                nn.Conv2d(image_size, image_size*2, kernel_size=4,
+                          stride=2, padding=1),
+                nn.LeakyReLU(0.1, inplace=True))
+
+        self.layer3 = nn.Sequential(
+                nn.Conv2d(image_size*2, image_size*4, kernel_size=4,
+                          stride=2, padding=1),
+                nn.LeakyReLU(0.1, inplace=True))
+
+        self.layer4 = nn.Sequential(
+                nn.Conv2d(image_size*4, image_size*8, kernel_size=4,
+                          stride=2, padding=1),
+                    nn.LeakyReLU(0.1, inplace=True))
+
+        self.last = nn.Conv2d(image_size*8, 1, kernel_size=4, stride=1)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.last(out)
+
+        return out
